89f0328b8a9a52aa5600a3007faae8402c48f38e
Meet240903 <meetmangroliya24@gmail.com>
2024-10-17 17:23:04 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import headerLogo from '../assets/images/headerLogo.svg'
+
+const Header = () => {
+    return (
+        <>
+            <h1>header</h1>
+        </>
+    )
+}
+
+export default Header
